02f8fcd092f57923200d94d32fad4b6b20951a2e
Phisten <ans10528@gmail.com>
2022-12-03 17:54:24 +0800

    feat: mock-images page swr+axios demo

--- web-nx/apps/nextjs/next.config.js
+++ web-nx/apps/nextjs/next.config.js
@@ -20,6 +20,12 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'unsplash.com',
+        port: '',
+        pathname: '/photos/**',
+      },
     ],
   },
 };
--- web-nx/apps/nextjs/pages/mock-images.tsx
+++ web-nx/apps/nextjs/pages/mock-images.tsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import Image from 'next/image';
 import { Fragment } from 'react';
 import useSWR from 'swr';
@@ -5,30 +6,43 @@ import useSWR from 'swr';
 /* eslint-disable-next-line */
 export interface MockImagesProps {}
 
+type Data = typeof DataMock[];
+const DataMock = {
+  author: 'Alejandro Escamilla',
+  download_url: 'https://picsum.photos/id/0/5000/3333',
+  height: 3333,
+  id: '0',
+  url: 'https://unsplash.com/photos/yC-Yzbqy7PY',
+  width: 5000,
+};
+
 export function MockImages(props: MockImagesProps) {
+  const { data, error } = useSWR<Data>('https://picsum.photos/v2/list', (url) =>
+    axios.get(url).then((res) => res.data)
+  );
 
-
-  const { data } = useSWR('https://picsum.photos/v2/list')
+  console.log({ data });
 
   return (
     <div>
       <h1 className="text-2xl">Welcome to MockImages!</h1>
-      {[1, 2, 3].map((x) => {
-        return (
-          <Fragment key={`${x}`}>
-            <div className="">
-              <Image
-                src={'https://picsum.photos/400/200'}
-                unoptimized
-                alt=""
-                // fill=''
-                width={400}
-                height={200}
-              />
-            </div>
-          </Fragment>
-        );
-      })}
+      <div className="grid grid-cols-3 mx-auto gap-4 max-w-[1232px]">
+        {data?.map((image) => {
+          return (
+            <Fragment key={`${image.id}`}>
+              <div className="">
+                <Image
+                  src={image.download_url}
+                  unoptimized
+                  alt=""
+                  width={400}
+                  height={200}
+                />
+              </div>
+            </Fragment>
+          );
+        })}
+      </div>
     </div>
   );
 }
