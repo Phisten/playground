424d79292bf1dd7d678cd977de8cd95c1732cda6
Phisten <ans10528@gmail.com>
2022-12-07 20:45:47 +0800

    feat: add debounce todo page

---
+++ web-nx/apps/nextjs/pages/debounce.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable-next-line */
+export interface DebounceProps {}
+
+export function Debounce(props: DebounceProps) {
+  return (
+    <div>
+      <h1>Welcome to Debounce!</h1>
+    </div>
+  );
+}
+
+export default Debounce;
