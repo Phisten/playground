58dea0a0311a4284e5adecea9037ece20e51209c
Phisten <ans10528@gmail.com>
2022-12-09 18:17:16 +0800

    feat(nextjs): notion demo init

---
+++ web-nx/apps/nextjs/pages/notion.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable-next-line */
+export interface NotionProps {}
+
+export function Notion(props: NotionProps) {
+  return (
+    <div>
+      <h1>Welcome to Notion!</h1>
+    </div>
+  );
+}
+
+export default Notion;
