d68cb302fcddbedf9d0034ff90cf6306424fb9ea
Phisten <ans10528@gmail.com>
2023-01-15 12:54:54 +0800

    chore: refactor folder

---
+++ csharp/Solution.cs
@@ -0,0 +1,15 @@
+public class Solution
+{
+    //leetCode 18: 4Sum
+    public IList<IList<int>> FourSum(int[] nums, int target)
+    {
+        List<IList<int>> opt = new List<IList<int>>();
+        opt.Add(new List<int>());
+        System.Console.WriteLine("input nums length: " + nums.Length);
+
+        List<int> inputNums = new List<int>(nums);
+        inputNums.Sort();
+
+        return opt;
+    }
+}
