07de424a51bb1618a19622cb1340af2fa3a41af8
Phisten <ans10528@gmail.com>
2023-01-15 12:34:43 +0800

    chore: workspace config

--- codefamer/cshape/Program.cs
+++ codefamer/cshape/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+Solution solution = new Solution();
+
+solution.FourSum(new int[] { 1, 0, -1, 0, -2, 2 }, 0);
--- codefamer/cshape/Solution.cs
+++ codefamer/cshape/Solution.cs
@@ -1,7 +1,6 @@
-using System.Collections;
-
 public class Solution
 {
+  //leetCode 18: 4Sum
   public IList<IList<int>> FourSum(int[] nums, int target)
   {
     List<IList<int>> opt = new List<IList<int>>();
