403f4ed5bbecfca4546d075c81fbb9d1553d8d58
Phisten <ans10528@gmail.com>
2023-01-23 20:18:24 +0800

    feat: cs test

--- csharp/Solution.cs
+++ csharp/Solution.cs
@@ -3,13 +3,51 @@ public class Solution
     //leetCode 18: 4Sum
     public IList<IList<int>> FourSum(int[] nums, int target)
     {
-        List<IList<int>> opt = new List<IList<int>>();
-        opt.Add(new List<int>());
+        var collect = new List<IList<int>>();
+        // opt.Add(new List<int>());
         System.Console.WriteLine("input nums length: " + nums.Length);
 
-        List<int> inputNums = new List<int>(nums);
+        // 遞增排序
+        var inputNums = new List<int>(nums);
         inputNums.Sort();
 
-        return opt;
+        var test = FourSumSubFunc(collect, inputNums.ToArray(), target, 0, 0);
+        System.Console.WriteLine(test);
+
+        return collect;
+    }
+
+    static public List<int> FourSumSubFunc(
+        IList<IList<int>> collect,
+        int[] sortedNums,
+        int target,
+        int startIndex,
+        // List<int> previousSelectNums,
+        int lastSum
+    )
+    {
+        // 終止條件
+        if (startIndex >= sortedNums.Length)
+        {
+            return new List<int>();
+        }
+        else
+        {
+            for (int i = startIndex; i < sortedNums.Length; i++)
+            {
+                var currentNumber = sortedNums[i];
+                var currentSum = lastSum + currentNumber;
+                if (currentSum < target)
+                {
+                    var next = FourSumSubFunc(collect, target, i + 1, currentSum);
+                }
+                else if (currentSum == target)
+                {
+                    collect.Add(previousSelectNums.Add(currentNumber));
+                }
+            }
+        }
+
+        // var subIssue = FourSum(inputNums.ToArray(), )
     }
 }
