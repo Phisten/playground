4cfd390d0402162109d8ddbbfa55cb2c3d12783c
Phisten <ans10528@gmail.com>
2023-01-25 01:21:39 +0800

    refactor: puppeteer

--- pttr/src/index.ts
+++ pttr/src/index.ts
@@ -1,22 +1,4 @@
-import puppeteer from "puppeteer-extra";
-import { executablePath } from "puppeteer";
-import StealthPlugin from "puppeteer-extra-plugin-stealth";
-
-const puppeteer_demo = () => {
-  puppeteer
-    .use(StealthPlugin())
-    .launch({
-      headless: true,
-      executablePath: executablePath(),
-    })
-    .then(async (browser) => {
-      const page = await browser.newPage();
-      await page.goto("https://bot.sannysoft.com");
-      await page.waitForTimeout(5000);
-      await page.screenshot({ path: "stealth.png", fullPage: true });
-      await browser.close();
-    });
-};
+import { puppeteer_demo } from "./puppeteer_demo";
 
 console.log("init puppeteer_demo");
 puppeteer_demo();
