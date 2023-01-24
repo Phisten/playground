0bf4d6a86b5e2eeba4e9d3a3ff58795020967fe1
Phisten <ans10528@gmail.com>
2023-01-24 23:58:57 +0800

    feat: puppeteer_demo

--- pttr/src/index.ts
+++ pttr/src/index.ts
@@ -1,21 +1,23 @@
 import puppeteer from "puppeteer-extra";
+import { executablePath } from "puppeteer";
 import StealthPlugin from "puppeteer-extra-plugin-stealth";
 
-// require executablePath from puppeteer
-const { executablePath } = require("puppeteer");
+const puppeteer_demo = () => {
+  puppeteer
+    .use(StealthPlugin())
+    .launch({
+      headless: true,
+      executablePath: executablePath(),
+    })
+    .then(async (browser) => {
+      const page = await browser.newPage();
+      await page.goto("https://bot.sannysoft.com");
+      await page.waitForTimeout(5000);
+      await page.screenshot({ path: "stealth.png", fullPage: true });
+      await browser.close();
+    });
+};
 
-puppeteer
-  .use(StealthPlugin())
-  .launch({
-    headless: true,
-
-    // add this
-    executablePath: executablePath(),
-  })
-  .then(async (browser) => {
-    const page = await browser.newPage();
-    await page.goto("https://bot.sannysoft.com");
-    await page.waitForTimeout(5000);
-    await page.screenshot({ path: "stealth.png", fullPage: true });
-    await browser.close();
-  });
+console.log("init puppeteer_demo");
+puppeteer_demo();
+console.log("end");
