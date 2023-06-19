eb439141f1feddd70e2f6fe2844556e4bcec3905
Phisten <ans10528@gmail.com>
2023-06-19 19:21:05 +0800

    add strapi

---
+++ strapi/src/api/paragraph/controllers/paragraph.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * paragraph controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::paragraph.paragraph');
---
+++ strapi/src/api/paragraph/services/paragraph.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * paragraph service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::paragraph.paragraph');
