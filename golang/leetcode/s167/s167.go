58099dfacbf39759786373a19507f131f5068770
Phisten <ans10528@gmail.com>
2023-08-20 22:10:17 +0800

    feat(go): s167 binarysearch

--- golang/leetcode/s167/s167.go
+++ golang/leetcode/s167/s167.go
@@ -11,18 +11,37 @@ func main() {
 	fmt.Println(twoSum([]int{-1, 0}, -1))
 }
 
+func binarySearch(nums []int, start int, end int, target int) int {
+
+	for start <= end {
+		mid := (end-start)/2 + start
+		midValue := nums[mid]
+
+		if midValue == target {
+			return mid
+		} else if midValue < target {
+			start = mid + 1
+		} else {
+			end = mid - 1
+		}
+	}
+
+	return -1
+}
+
 func twoSum(numbers []int, target int) []int {
+
 	numbersLen := len(numbers)
 
 	leftIdx := 0
+	endIdx := numbersLen - 1
 
-	for leftIdx < numbersLen-1 {
-		if numbers[leftIdx]+numbers[numbersLen-1] >= target {
+	for leftIdx < endIdx {
+		if numbers[leftIdx]+numbers[endIdx] >= target {
 
-			for rightIdx := leftIdx + 1; rightIdx < numbersLen; rightIdx += 1 {
-				if numbers[leftIdx]+numbers[rightIdx] == target {
-					return []int{leftIdx + 1, rightIdx + 1}
-				}
+			tryFindIdx := binarySearch(numbers, leftIdx+1, endIdx, target-numbers[leftIdx])
+			if tryFindIdx >= 0 {
+				return []int{leftIdx + 1, tryFindIdx + 1}
 			}
 		}
 		leftIdx += 1
