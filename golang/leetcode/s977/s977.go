4da25c0d038e1e228dd5e85eb3cce11e334b7ef1
Phisten <ans10528@gmail.com>
2023-09-23 23:32:53 +0800

    feat(go): s977 O(n) 2

--- golang/leetcode/s977/s977.go
+++ golang/leetcode/s977/s977.go
@@ -9,7 +9,6 @@ func main() {
 	fmt.Printf("%v -> [0,1,9,16,100] \n", sortedSquares([]int{-4, -1, 0, 3, 10}))
 }
 
-// O(N
 func sortedSquares(nums []int) []int {
 	n := len(nums)
 	left := 0
@@ -25,6 +24,7 @@ func sortedSquares(nums []int) []int {
 			ans[i] = leftSq
 			left++
 		} else {
+
 			leftSq := nums[left] * nums[left]
 			rightSq := nums[right] * nums[right]
 			if leftSq > rightSq {
@@ -36,6 +36,41 @@ func sortedSquares(nums []int) []int {
 			}
 		}
 
+	}
+	return ans
+}
+
+// O(N
+func sortedSquares_0(nums []int) []int {
+
+	for i, v := range nums {
+		if v >= 0 {
+			break
+		} else {
+			nums[i] = -v
+		}
+	}
+
+	n := len(nums)
+	left := 0
+	right := n - 1
+	ans := make([]int, n)
+	for i := right; i >= 0; i-- {
+		if left == n-1 {
+			ans[i] = nums[right] * nums[right]
+			right--
+		} else if right == 0 {
+			ans[i] = nums[left] * nums[left]
+			left++
+		} else {
+			if nums[left] > nums[right] {
+				ans[i] = nums[left] * nums[left]
+				left++
+			} else {
+				ans[i] = nums[right] * nums[right]
+				right--
+			}
+		}
 	}
 	return ans
 }
