82c0968c77363c8ba3b18ef95db8a50292276d83
Phisten <ans10528@gmail.com>
2023-10-06 04:36:51 +0800

    fix(go): s238 Time Limit Exceeded

--- golang/leetcode/s238/s238.go
+++ golang/leetcode/s238/s238.go
@@ -8,18 +8,24 @@ func main() {
 	fmt.Printf("%v -> [24,12,8,6]  \n", productExceptSelf([]int{1, 2, 3, 4}))
 }
 
+// 首先想到將所有數相乘, 然後掃描nums時拿總乘積除以nums[i]得到每一位的答案
+//   然後才發現題目要求不能用除法運算子
+
+// O(N^2)乘法掃描方法不符合題目速度要求O(N)
 func productExceptSelf(nums []int) []int {
 
-	product := 1
-	for _, v := range nums {
-		product *= v
-	}
-	for i := range nums {
-		if nums[i] != 0 {
-			nums[i] = product / nums[i]
-		} else {
-			nums[i] = product
+	n := len(nums)
+	ans := make([]int, n)
+
+	for i := range ans {
+		ans[i] = 1
+
+		for j, q := range nums {
+			if i != j {
+				ans[i] *= q
+			}
 		}
 	}
-	return nums
+
+	return ans
 }
