158c6e5d4cfa325511a7996f96f619ed7f3c1df2
Phisten <ans10528@gmail.com>
2023-10-14 22:38:44 +0800

    feat(go): c1

---
+++ golang/leetcode/c1/c1.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	fmt.Printf("%v -> [2,1,-1] \n", lastVisitedIntegers([]string{"1", "2", "prev", "prev", "prev"}))
+}
+func lastVisitedIntegers(words []string) []int {
+
+	ans := []int{}
+
+	ints := []int{}
+
+	prevStream := 0
+
+	for _, v := range words {
+		if v != "prev" {
+			marks, _ := strconv.Atoi(v)
+			ints = append(ints, marks)
+			prevStream = 0
+		} else {
+			prevStream++
+			n := len(ints)
+			if n >= prevStream {
+				ans = append(ans, ints[len(ints)-prevStream])
+			} else {
+				ans = append(ans, -1)
+			}
+		}
+	}
+
+	return ans
+}
