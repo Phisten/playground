5f9e4626f5878f2da30ddf2419e36ce573a45b7d
Phisten <ans10528@gmail.com>
2023-11-13 08:22:21 +0800

    feat(go): leetcode treeNode preset

---
+++ golang/leetcode/leetcode/leetcode.go
@@ -0,0 +1,40 @@
+package leetcode
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func ArrayToTreeNode(arr []interface{}) *TreeNode {
+	if len(arr) == 0 {
+		return nil
+	}
+
+	root := &TreeNode{}
+	queue := []*TreeNode{root}
+	index := 0
+
+	for len(queue) > 0 {
+		node := queue[0]
+		queue = queue[1:]
+
+		if arr[index] != nil {
+			val, _ := arr[index].(int)
+			node.Val = val
+			index++
+			node.Left = &TreeNode{}
+			queue = append(queue, node.Left)
+		}
+
+		if index < len(arr) && arr[index] != nil {
+			val, _ := arr[index].(int)
+			node.Val = val
+			index++
+			node.Right = &TreeNode{}
+			queue = append(queue, node.Right)
+		}
+	}
+
+	return root
+}
