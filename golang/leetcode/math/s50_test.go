1c89791d7ae85bcce43425660e5b1954a7bcf4bd
Phisten <ans10528@gmail.com>
2023-11-15 20:07:02 +0800

    refactor(go): s50

--- golang/leetcode/math/s50_test.go
+++ golang/leetcode/math/s50_test.go
@@ -5,22 +5,27 @@ import (
 	"testing"
 )
 
-func round5(f float64) float64 {
-	return math.Round(f*10000.0) / 10000
-}
-
-func Test(t *testing.T) {
-
-	res, ans := round5(myPow(2.0, 10)), 1024.0
-	if res != ans {
-		t.Errorf("Expected %v, but got %v", ans, res)
+func Test_s50(t *testing.T) {
+	round5 := func(f float64) float64 {
+		return math.Round(f*10000.0) / 10000
 	}
-	res, ans = round5(myPow(2.1, 3)), 9.261
-	if res != ans {
-		t.Errorf("Expected %v, but got %v", ans, res)
+
+	if got, exp := round5(myPow(2.0, 10)), 1024.0; got != exp {
+		t.Errorf("Expected %v, but got %v", exp, got)
+	}
+	if got, exp := round5(myPow(2.1, 3)), 9.261; got != exp {
+		t.Errorf("Expected %v, but got %v", exp, got)
+	}
+
+	if got, exp := round5(myPow_sdk(2.1, 3)), 9.261; got != exp {
+		t.Errorf("myPow_sdk Expected %v, but got %v", exp, got)
 	}
 }
 
 func myPow(x float64, n int) float64 {
+	return math.Pow(x, float64(n))
+}
+
+func myPow_sdk(x float64, n int) float64 {
 	return math.Pow(x, float64(n))
 }
