3500ccb8e62d9d3b96fce1123244a6cb79dda9bd
Phisten <ans10528@gmail.com>
2023-11-15 20:00:26 +0800

    feat(go): s50 with test

--- golang/leetcode/math/s50_test.go
+++ golang/leetcode/math/s50_test.go
@@ -1,21 +1,26 @@
 package leetcode
 
 import (
+	"math"
 	"testing"
 )
 
+func round5(f float64) float64 {
+	return math.Round(f*10000.0) / 10000
+}
+
 func Test(t *testing.T) {
 
-	res, ans := myPow(2.0, 10), 1024.0
+	res, ans := round5(myPow(2.0, 10)), 1024.0
 	if res != ans {
 		t.Errorf("Expected %v, but got %v", ans, res)
 	}
-	res, ans = myPow(2.1, 3), 9.261
+	res, ans = round5(myPow(2.1, 3)), 9.261
 	if res != ans {
 		t.Errorf("Expected %v, but got %v", ans, res)
 	}
 }
 
 func myPow(x float64, n int) float64 {
-	return 0
+	return math.Pow(x, float64(n))
 }
