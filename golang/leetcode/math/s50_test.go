d94d87044298f1d5a6f1783cff79e1995bacdd75
Phisten <ans10528@gmail.com>
2023-11-16 11:16:34 +0800

    feat(go): s50 O(n)

--- golang/leetcode/math/s50_test.go
+++ golang/leetcode/math/s50_test.go
@@ -6,6 +6,8 @@ import (
 	"testing"
 )
 
+var Pnt = fmt.Printf
+
 func Test_s50(t *testing.T) {
 	fmt.Printf("Test_s50\n")
 	round5 := func(f float64) float64 {
@@ -37,10 +39,12 @@ func Test_s50(t *testing.T) {
 			{Func.call(2.0, 10), 1024.0, ""},
 			{Func.call(2.1, 3), 9.261, ""},
 			{Func.call(2.0, -2), 0.25, ""},
-			{Func.call(2.0, 0), 1.0, ""},
-			{Func.call(8.95371, -1), 0.1117, ""},
+			// {Func.call(2.0, 0), 1.0, ""},
+			// {Func.call(8.95371, -1), 0.1117, ""},
 			{Func.call(1.84364, -14), 0.0002, ""},
-			{Func.call(-13.62608, 3), -2529.955, ""},
+			// {Func.call(-13.62608, 3), -2529.955, ""},
+			// {Func.call(0.00001, 2147483647), 0, ""},
+			// {Func.call(1.0, -2147483648), 1, ""},
 		}
 
 		for _, v := range paris {
@@ -51,14 +55,51 @@ func Test_s50(t *testing.T) {
 	}
 }
 
+/*
+*
+ */
+func myPow(x float64, n int) float64 {
+	// Pnt("x=%v, n=%v\n", x, n)
+	if x == 0 {
+		return 0
+	}
+	if n == 0 {
+		return 1
+	}
+
+	negative := n < 0
+	if negative {
+		n = -n
+	}
+	ans := 1.0
+
+	for i := n; i > 0; i >>= 1 {
+		odd := i & 1
+
+		if odd == 1 {
+			ans *= x
+		}
+		x *= x
+
+		// Pnt("nLast=%v, ans=%v\n", i, ans)
+	}
+
+	if negative {
+		ans = 1.0 / ans
+	}
+
+	return ans
+}
+
 // 1 2 4 8 16 32 64 128 256 512 1024
 // 0 1 2 3 4  5  6  7   8   9   10
 // 2^3^2 = 8*8 = 64 = 2^6
 //
 // 64 16 4 2 1.4
 // 1  -1 2 3 4
-func myPow(x float64, n int) float64 {
-	// fmt.Printf("> x=%v, n=%v\n", x, n)
+func myPow_OOM(x float64, n int) float64 {
+	// var Pnt = func(...interface{}) {}
+	Pnt("> x=%v, n=%v\n", x, n)
 	if n == 0 {
 		return 1
 	}
@@ -66,27 +107,27 @@ func myPow(x float64, n int) float64 {
 	if negative {
 		n = -n
 	}
-	dp := make([]float64, n+1)
+	dp := make([]float64, 33)
 
 	lastN := n
 	ans := x
 	dp[1] = x
 
-	for i := 1; i <= n/2; {
-		ans = dp[i] * dp[i]
+	for i, p := 1, 0; i <= n/2; p++ {
+		ans = dp[p] * dp[p]
 
 		i *= 2
 		dp[i] = ans
 		lastN = n - i
-		// fmt.Printf("i=%v, lastN=%v, ans=%v\n", i, lastN, ans)
+		Pnt("i=%v, lastN=%v, ans=%v\n", i, lastN, ans)
 	}
 
 	if n != 1 {
 		for i := lastN; i >= 1; {
-			if dp[i] > 0 {
+			if dp[i] != 0 {
 				ans *= dp[i]
 				lastN -= i
-				// fmt.Printf("i=%v, lastN=%v, ans=%v\n", i, lastN, ans)
+				Pnt("i=%v, lastN=%v, ans=%v\n", i, lastN, ans)
 				i = lastN
 			} else {
 				i--
