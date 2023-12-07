f0ab2cb42447f462e1cb74418193aff8d2c592cf
Phisten <ans10528@gmail.com>
2023-12-08 04:49:15 +0800

    chore(nestjs): change port

--- nestjs/src/main.ts
+++ nestjs/src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(MessagesModule);
   app.useGlobalPipes(new ValidationPipe());
-  const port = process.env.PORT || 3000;
+  const port = process.env.PORT || 3500;
 
   await app.listen(port);
 }
