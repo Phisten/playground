8f3445c6f5755da4f6c079a07832c8978ca823c0
Phisten <ans10528@gmail.com>
2023-12-11 06:32:04 +0800

    chore: move nestjs project

--- nestjs/di/src/disk/disk.module.ts
+++ nestjs/di/src/disk/disk.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { DiskService } from './disk.service';
-import { DiskController } from './disk.controller';
+import { PowerModule } from 'src/power/power.module';
 
 @Module({
+  imports: [PowerModule],
   providers: [DiskService],
-  controllers: [DiskController]
+  exports: [DiskService],
 })
 export class DiskModule {}
--- nestjs/di/src/disk/disk.service.ts
+++ nestjs/di/src/disk/disk.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { PowerService } from 'src/power/power.service';
 
 @Injectable()
-export class DiskService {}
+export class DiskService {
+  constructor(private powerService: PowerService) {}
+
+  getDate() {
+    console.log('Drawing 3 watts of power from Power Service');
+    this.powerService.supplyPower(3);
+    return 'data';
+  }
+}
