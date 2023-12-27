c4669cbe422481ceb91050414099348acea181e1
Phisten <ans10528@gmail.com>
2023-12-27 22:38:11 +0800

    feat(nest): update user pwd

---
+++ nestjs/car-value/src/users/dtos/update-user.dto.ts
@@ -0,0 +1,11 @@
+import { IsEmail, IsOptional, IsString } from 'class-validator';
+
+export class UpdateUserDto {
+  @IsOptional()
+  @IsEmail()
+  email: string;
+
+  @IsOptional()
+  @IsString()
+  pwd: string;
+}
