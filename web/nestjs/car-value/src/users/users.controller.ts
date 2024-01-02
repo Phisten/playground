1265fd7884e8b70d33f26739a529001fb6677cff
Phisten <ans10528@gmail.com>
2024-01-02 10:12:50 +0800

    feat(nest): global interceptor:user info

--- web/nestjs/car-value/src/users/users.controller.ts
+++ web/nestjs/car-value/src/users/users.controller.ts
@@ -18,10 +18,8 @@ import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
 import { AuthService } from './auth.service';
 import { CurrentUser } from './decorators/current-user.decorator';
-import { CurrentUserInterceptor } from './interceptors/current-user.interceptor';
 import { User } from './users.entity';
 
-@UseInterceptors(CurrentUserInterceptor)
 @Serialize(UserDto)
 @Controller('auth')
 export class UsersController {
--- web/nestjs/car-value/src/users/users.module.ts
+++ web/nestjs/car-value/src/users/users.module.ts
@@ -5,10 +5,15 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './users.entity';
 import { AuthService } from './auth.service';
 import { CurrentUserInterceptor } from './interceptors/current-user.interceptor';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
-  providers: [UsersService, AuthService, CurrentUserInterceptor],
+  providers: [
+    UsersService,
+    AuthService,
+    { provide: APP_INTERCEPTOR, useClass: CurrentUserInterceptor },
+  ],
 })
 export class UsersModule {}
