9b502455e2a0b1c8db6664e70753d4d6065f05d0
Phisten <ans10528@gmail.com>
2024-01-03 17:02:16 +0800

    feat(nest): auth test service

--- web/nestjs/car-value/src/users/auth.service.spec.ts
+++ web/nestjs/car-value/src/users/auth.service.spec.ts
@@ -1,8 +1,25 @@
 import { Test } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { UsersService } from './users.service';
 
 it('can create an instance of auth service', async () => {
+  // create a fake copy of the user service
+  const fakeUsersService = {
+    createUser: (email: string, password: string) =>
+      Promise.resolve({ id: 1, email, password }),
+    find: () => Promise.resolve([]),
+  };
+
   const module = await Test.createTestingModule({
-    providers: [AuthService],
+    providers: [
+      AuthService,
+      {
+        provide: UsersService,
+        useValue: fakeUsersService,
+      },
+    ],
   }).compile();
+
+  const service = module.get(AuthService);
+  expect(service).toBeDefined();
 });
