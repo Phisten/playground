08ea31c4731dc4381515b70f4b6ee493d4415c75
Phisten <ans10528@gmail.com>
2024-01-04 12:20:16 +0800

    feat(go): s268

---
+++ golang/leetcode/HashTable/s268.go
@@ -0,0 +1,15 @@
+func missingNumber(nums []int) int {
+	l := len(nums)
+	ht := make(map[int]bool, l)
+
+	for _, v := range nums {
+		ht[v] = true
+	}
+
+	for i := 0; i <= l; i++ {
+		if ht[i] == false {
+			return i
+		}
+	}
+	return 0
+}
