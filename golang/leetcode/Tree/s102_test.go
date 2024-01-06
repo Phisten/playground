283c94c97b115a31add8e662feaf85896c9d6ec9
Phisten <ans10528@gmail.com>
2024-01-07 00:46:47 +0800

    feat(go): 102

--- golang/leetcode/Tree/s102_test.go
+++ golang/leetcode/Tree/s102_test.go
@@ -16,19 +16,24 @@ type Node struct {
 
 func levelOrder(root *TreeNode) [][]int {
 	ans := [][]int{}
+
 	if root == nil {
 		return ans
 	}
 
 	ans = append(ans, []int{})
 	queue := []Node{{root, 0}}
+
 	for len(queue) > 0 {
 		cur := queue[0]
 		queue = queue[1:]
+
 		if cur.node != nil {
+
 			if cur.level == len(ans) {
 				ans = append(ans, []int{})
 			}
+
 			ans[cur.level] = append(ans[cur.level], cur.node.Val)
 
 			queue = append(queue,
