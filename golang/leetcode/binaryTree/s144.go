7b1a4cd4124173adcedb9933425cb892fb60224a
Phisten <ans10528@gmail.com>
2024-01-11 01:41:19 +0800

    feat(go): s144

---
+++ golang/leetcode/binaryTree/s144.go
@@ -0,0 +1,26 @@
+package leetcode
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func preorderTraversal(root *TreeNode) []int {
+	ans := []int{}
+
+	var recursion func(node *TreeNode)
+	recursion = func(node *TreeNode) {
+		if node != nil {
+			ans = append(ans, node.Val)
+			recursion(node.Left)
+			recursion(node.Right)
+		}
+	}
+	recursion(root)
+
+	return ans
+}
