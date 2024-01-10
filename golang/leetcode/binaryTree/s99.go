16b840a033686d5beeacdfbcd540710ab23bc3ff
Phisten <ans10528@gmail.com>
2024-01-11 00:39:15 +0800

    feat(go): BST s99

---
+++ golang/leetcode/binaryTree/s99.go
@@ -0,0 +1,51 @@
+package leetcode
+
+/**
+* Definition for a binary tree node.
+* type TreeNode struct {
+*     Val int
+*     Left *TreeNode
+*     Right *TreeNode
+* }
+
+
+BST中序走訪就是遞增序
+紀錄前一個點的位置中序走訪就可以找到錯誤的點
+找到第一組錯誤時外部緩存兩個節點（第二個不一定錯）
+繼續往後找有無第二組錯誤, 有就蓋掉第二個緩存的節點
+全部訪問後交換
+
+只有兩個節點錯置
+第一組錯誤搜到 第一個節點一定是錯的
+若搜到第二組錯誤 第二組的第二格節點是錯的
+*/
+
+func recoverTree(root *TreeNode) {
+	var prevNode, incorrect1, incorrect2 *TreeNode
+
+	var recursion func(cur *TreeNode)
+	recursion = func(cur *TreeNode) {
+		if cur == nil {
+			return
+		}
+
+		recursion(cur.Left)
+
+		if prevNode != nil {
+			if prevNode.Val > cur.Val {
+				if incorrect1 == nil {
+					incorrect1 = prevNode
+				}
+				if incorrect1 != nil {
+					incorrect2 = cur
+				}
+			}
+		}
+		prevNode = cur
+
+		recursion(cur.Right)
+	}
+
+	recursion(root)
+	incorrect1.Val, incorrect2.Val = incorrect2.Val, incorrect1.Val
+}
