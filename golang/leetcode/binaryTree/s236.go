6fd4ed96026130718f3b1799f0c56dc7543b9a8c
Phisten <ans10528@gmail.com>
2024-01-11 05:33:59 +0800

    feat(go): s236

---
+++ golang/leetcode/binaryTree/s236.go
@@ -0,0 +1,41 @@
+package leetcode
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+
+	var dfs func(node *TreeNode) (*TreeNode, bool, bool)
+	dfs = func(node *TreeNode) (*TreeNode, bool, bool) {
+		if node == nil {
+			return nil, false, false
+		}
+
+		left, pInLeft, qInLeft := dfs(node.Left)
+		if pInLeft && qInLeft {
+			return left, pInLeft, qInLeft
+		}
+		right, pInRight, qInRight := dfs(node.Right)
+		if pInRight && qInRight {
+			return right, pInRight, qInRight
+		}
+
+		if (pInLeft || pInRight) && (qInLeft || qInRight) {
+			return node, true, true
+		}
+
+		var ans *TreeNode
+		if p == node || q == node {
+			ans = node
+		}
+
+		return ans, p == node || pInLeft || pInRight, q == node || qInLeft || qInRight
+	}
+	lca, _, _ := dfs(root)
+	return lca
+}
