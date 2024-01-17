b0558da0bb9fac69efc439f7e04c3692d739d1e6
Phisten <ans10528@gmail.com>
2024-01-18 03:07:27 +0800

    leetcode s322 dp

--- leetcode/322.coin-change.py
+++ leetcode/322.coin-change.py
@@ -11,7 +11,7 @@ from typing import List
 
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        dp = []
+        dp: dict = {}
 
         def dfs(last_amount: int) -> int:
             res = float("inf")
@@ -19,9 +19,12 @@ class Solution:
                 return 0
             elif last_amount < 0:
                 None
+            elif last_amount in dp:
+                return dp[last_amount]
             else:
                 for v in coins:
                     res = min(res, dfs(last_amount - v) + 1)
+                dp[last_amount] = res
 
             return res
 
