86ef5e640740836a9c3388cb1b529e3f8b893b50
Phisten <ans10528@gmail.com>
2024-02-03 01:59:42 +0800

    leetcode s274 ts

--- leetcode/typescript/s274_h-index.spec.ts
+++ leetcode/typescript/s274_h-index.spec.ts
@@ -2,23 +2,26 @@
 describe('274. H-Index', () => {
 
   it('1', () => expect(hIndex([3, 0, 6, 1, 5])).toEqual(3))
+  // 0 1 3 5 6
   it('2', () => expect(hIndex([1, 3, 1])).toEqual(1))
+  it('3', () => expect(hIndex([100])).toEqual(1))
+  it('4', () => expect(hIndex([0])).toEqual(0))
+  // 3 3 4 4 6 6 8 10
+  // 8 7 6 5 4 3 2 1
+  it('5', () => expect(hIndex([6, 6, 4, 8, 4, 3, 3, 10])).toEqual(4))
 })
 
 
-// 0 1 3 5 6
 function hIndex(citations: number[]): number {
   const len = citations.length;
-  if (len === 1) {
-    return citations[0];
-  }
 
-  citations.sort();
-  for (let i = 1; i <= len; i++) {
-    const ri = len - i;
-    const v = citations[ri];
-    if (v > ri) {
-      return ri + 1
+  citations.sort((a, b) => a - b);
+
+  for (let i = 0; i < len; i++) {
+    const target = len - i;
+    const v = citations[i];
+    if (v >= target) {
+      return target;
     }
   }
 
