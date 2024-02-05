7f90ef8aba61d0d3b026e0dd300ddf54ed49cbe9
Phisten <ans10528@gmail.com>
2024-02-06 07:54:10 +0800

    leetcode Treenode, ListNode ts

--- leetcode/109.convert-sorted-list-to-binary-search-tree.ts
+++ leetcode/109.convert-sorted-list-to-binary-search-tree.ts
@@ -1,30 +1,8 @@
 /*
  * @lc app=leetcode id=109 lang=typescript
- *
- * [109] Convert Sorted List to Binary Search Tree
  */
 
 // @lc code=start
-
-class ListNode {
-  val: number
-  next: ListNode | null
-  constructor(val?: number, next?: ListNode | null) {
-    this.val = (val === undefined ? 0 : val)
-    this.next = (next === undefined ? null : next)
-  }
-}
-
-class TreeNode {
-  val: number
-  left: TreeNode | null
-  right: TreeNode | null
-  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
-    this.val = (val === undefined ? 0 : val)
-    this.left = (left === undefined ? null : left)
-    this.right = (right === undefined ? null : right)
-  }
-}
 
 function sortedListToBST(head: ListNode | null): TreeNode | null {
   if (!head) return null;
---
+++ leetcode/LeetcodeDataStruct.ts
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode lang=typescript
+ */
+
+// @lc code=start
+
+class ListNode {
+  val: number
+  next: ListNode | null
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = (val === undefined ? 0 : val)
+    this.next = (next === undefined ? null : next)
+  }
+}
+
+class TreeNode {
+  val: number
+  left: TreeNode | null
+  right: TreeNode | null
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = (val === undefined ? 0 : val)
+    this.left = (left === undefined ? null : left)
+    this.right = (right === undefined ? null : right)
+  }
+}
+
+// @lc code=end
