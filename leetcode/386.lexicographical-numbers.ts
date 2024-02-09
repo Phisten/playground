70b96e12343cfc017b18420bd5f80908590478d3
Phisten <ans10528@gmail.com>
2024-02-09 09:10:15 +0800

    leetcode ts s386 with comment

--- leetcode/386.lexicographical-numbers.ts
+++ leetcode/386.lexicographical-numbers.ts
@@ -7,23 +7,27 @@
 // @lc code=start
 function largestDivisibleSubset(nums: number[]): number[] {
   const length = nums.length
-
   nums.sort((a, b) => a - b);
 
+  // store the length of the largest divisible subset at each index
   const dp: number[] = Array.from<number>({ length }).fill(1);
-  dp[0] = 1;
-
+  // store the index of the previous element in the largest divisible subset
   const linked: number[] = Array.from<number>({ length });
+
+  // Variables to track the maximum subset length and the index of its head element
   let maxSubset = 1;
   let headIdx = 0;
 
+  // Dynamic Programming: Iterate through the array to find the largest divisible subset
   for (let i = 1; i < length; i++) {
     for (let j = 0; j < i; j++) {
       if (nums[i] % nums[j] == 0) {
+        // If nums[i] is divisible by nums[j], update the subset length and linked index
         if (dp[j] + 1 > dp[i]) {
           dp[i] = dp[j] + 1;
           linked[i] = j;
 
+          // Update the maximum subset length and linkedList head index
           if (dp[i] > maxSubset) {
             headIdx = i;
             maxSubset = dp[i];
@@ -33,7 +37,8 @@ function largestDivisibleSubset(nums: number[]): number[] {
     }
   }
 
-  const ans = [];
+  // Reconstruct the largest divisible subset based on linked indices
+  const ans: number[] = [];
   while (nums[headIdx]) {
     ans.push(nums[headIdx])
     headIdx = linked[headIdx];
