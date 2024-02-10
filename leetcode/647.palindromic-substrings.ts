a95ce7ead39627be4fe61f932742c73464058f26
Phisten <ans10528@gmail.com>
2024-02-10 23:48:45 +0800

    leetcode ts s647

---
+++ leetcode/647.palindromic-substrings.ts
@@ -0,0 +1,12 @@
+/*
+ * @lc app=leetcode id=647 lang=typescript
+ *
+ * [647] Palindromic Substrings
+ */
+
+// @lc code=start
+function countSubstrings(s: string): number {
+    
+};
+// @lc code=end
+
