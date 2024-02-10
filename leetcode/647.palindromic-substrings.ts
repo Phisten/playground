8b362edf893c83d3ded7b2fba3a6787cc95cb455
Phisten <ans10528@gmail.com>
2024-02-11 02:22:03 +0800

    leetcode ts s647 comment

--- leetcode/647.palindromic-substrings.ts
+++ leetcode/647.palindromic-substrings.ts
@@ -6,8 +6,33 @@
 
 // @lc code=start
 function countSubstrings(s: string): number {
+  // Expand substring width from central indices and calculate the count of palindromes.
+  const widthSearch = (l, r): number => {
+    let res = 0;
+    while (l >= 0 && r < s.length) {
+      if (s[l] !== s[r])
+        break;
+
+      res++; l--; r++;
+    }
+    return res;
+  }
+
+  let ans = widthSearch(0, 0);
+  // Enumerate all central indices of substrings.
+  for (let i = 1; i < s.length; i++) {
+    ans += widthSearch(i, i);
+    ans += widthSearch(i - 1, i);
+  }
+
+  return ans;
+};
+// @lc code=end
+
+function countSubstrings_bf(s: string): number {
   const length = s.length;
 
+  // Check if the substring is a palindrome.
   const isPalindromic = (start, end): boolean => {
     for (let i = 0; i <= end - start; i++)
       if (s[start + i] !== s[end - i])
@@ -17,8 +42,10 @@ function countSubstrings(s: string): number {
   }
 
   let ans = 0;
+  // Enumerate all substrings of varying widths and their starting indices.
   for (let width = 1; width <= length; width++) {
     for (let i = 0; i <= length - width; i++) {
+      // Increment the counter for palindrome substrings.
       if (isPalindromic(i, i + width - 1)) {
         ans++;
       }
@@ -27,4 +54,3 @@ function countSubstrings(s: string): number {
 
   return ans;
 };
-// @lc code=end
