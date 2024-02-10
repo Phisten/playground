a5c47b2108a462460c5b4e0d9abe496b91940f5f
Phisten <ans10528@gmail.com>
2024-02-11 00:15:31 +0800

    leetcode ts s647

--- leetcode/647.palindromic-substrings.ts
+++ leetcode/647.palindromic-substrings.ts
@@ -6,7 +6,25 @@
 
 // @lc code=start
 function countSubstrings(s: string): number {
-    
+  const length = s.length;
+
+  const isPalindromic = (start, end): boolean => {
+    for (let i = 0; i <= end - start; i++)
+      if (s[start + i] !== s[end - i])
+        return false
+
+    return true
+  }
+
+  let ans = 0;
+  for (let width = 1; width <= length; width++) {
+    for (let i = 0; i <= length - width; i++) {
+      if (isPalindromic(i, i + width - 1)) {
+        ans++;
+      }
+    }
+  }
+
+  return ans;
 };
 // @lc code=end
-
