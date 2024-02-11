0860f9ea8e338fdce84c217b03e28d5725ff64ef
Phisten <ans10528@gmail.com>
2024-02-11 11:24:23 +0800

    lc ts s1463 comment

--- leetcode/1463.cherry-pickup-ii.ts
+++ leetcode/1463.cherry-pickup-ii.ts
@@ -13,27 +13,34 @@ function cherryPickup(grid: number[][]): number {
     Array.from({ length: rows }, () => Array.from<number>({ length: rows }).fill(-1)));
 
   const dfs = (lastLb, lastRb, col): number => {
-
-
+    // Base case: If reached out of the last column, return 0 as no more cherries can be picked.
     if (col === cols) return 0;
+
+    // If the result for the current position is already calculated, return the cached value.
     if (dp[col][lastLb][lastRb] >= 0) return dp[col][lastLb][lastRb];
 
+    // Calculate the cherry value at the current path.
     const curValue = lastLb !== lastRb ? grid[col][lastLb] + grid[col][lastRb] : grid[col][lastLb];
-    let best = 0
+
+    // maximum cherry collection.
+    let best = 0;
+
+    // Iterate over possible leftRobot and rightRobot path in the next column.
     for (let l = lastLb - 1; l <= lastLb + 1; l++) {
       for (let r = lastRb - 1; r <= lastRb + 1; r++) {
+        // Check if the path are within bounds.
         if (l >= 0 && l < rows && r >= 0 && r < rows) {
+          // Recursively calculate the maximum cherry collection for the next column.
           best = Math.max(best, dfs(l, r, col + 1));
         }
       }
     }
 
-    // Cache the result for the current position and return the total cherry collection.
+    // Cache the result for the current path and return the total cherry collection.
     dp[col][lastLb][lastRb] = curValue + best;
-    return curValue + best;
+    return dp[col][lastLb][lastRb];
   };
 
-  const ans = dfs(0, rows - 1, 0);
-  return ans;
+  return dfs(0, rows - 1, 0);
 };
 // @lc code=end
