c021250c5fdcbe2eb5e3b60b1288fde2ce854e78
Phisten <ans10528@gmail.com>
2024-02-12 02:17:13 +0800

    OA lc ts s230 comment

--- leetcode/230.kth-smallest-element-in-a-bst.ts
+++ leetcode/230.kth-smallest-element-in-a-bst.ts
@@ -5,25 +5,30 @@
  */
 
 import { TreeNode } from './LeetcodeDataStruct';
-
 // @lc code=start
 function kthSmallest(root: TreeNode | null, k: number): number {
-
   let ans: number | null = null;
-  const dsf = (node: TreeNode | null) => {
+
+  // inorder traversal recursion
+  const dfs = (node: TreeNode | null) => {
+    // If the node is null or the result is already found, return
     if (node == null || ans !== null) return;
 
-    dsf(node.left);
+    dfs(node.left);
 
+    // Decrement the count of remaining nodes to visit
     k--;
+
+    // If k becomes 0, we found the kth smallest element
     if (k === 0) {
-      ans = node.val;
+      ans = node.val; // Store the value in the result variable
       return;
     }
 
-    dsf(node.right);
+    dfs(node.right);
   }
-  dsf(root);
+
+  dfs(root);
 
   return ans ?? 0;
 };
