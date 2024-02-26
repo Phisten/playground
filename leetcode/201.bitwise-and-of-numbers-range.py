b157accd4fccb89334439febd2a76459cdf4c026
Phisten <ans10528@gmail.com>
2024-02-26 13:47:07 +0800

    OA lc py3 s201

--- leetcode/201.bitwise-and-of-numbers-range.py
+++ leetcode/201.bitwise-and-of-numbers-range.py
@@ -5,30 +5,26 @@
 #
 
 
-# @lc code=startclass Solution:
-def rangeBitwiseAnd(self, left: int, right: int) -> int:
-    i = 1
-    l = left
-    r = right
+# @lc code=start
+class Solution:
+    def rangeBitwiseAnd(self, left: int, right: int) -> int:
+        i, diff = 1, right - left
 
-    while l > 0 and r > 0:
-        l = l >> 1
-        r = r >> 1
-        i = i << 1
-
-    if l == 0 and r == 0:
-        diff = abs(left - right)
-        i = 1
-        while diff > 0:
-            diff = diff >> 1
+        while i < diff:
             i = i << 1
 
-        if i > 0:
-            return (left - (left % i)) & (right - (right % i))
-        else:
-            return left
-    else:
-        return 0
+        return (left - left % i) & right
 
 
 # @lc code=end
+
+
+class Solution_offset:
+    def rangeBitwiseAnd(self, left: int, right: int) -> int:
+        i, diff = 0, right - left
+
+        while diff > 0:
+            diff = diff >> 1
+            i += 1
+
+        return (left >> i << i) & right
