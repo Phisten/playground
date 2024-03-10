b8ebaa8101ff74e19e36bd96dfd267bc3d1b49aa
Phisten <ans10528@gmail.com>
2024-03-10 23:03:01 +0800

    OA lc ts s863

---
+++ leetcode/typescript/s863.spec.ts
@@ -0,0 +1,47 @@
+
+describe('863. All Nodes Distance K in Binary Tree', () => {
+
+})
+
+class TreeNode {
+  val: number
+  left: TreeNode | null
+  right: TreeNode | null
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = (val === undefined ? 0 : val)
+    this.left = (left === undefined ? null : left)
+    this.right = (right === undefined ? null : right)
+  }
+}
+
+function distanceK(root: TreeNode | null, target: TreeNode | null, k: number): number[] {
+  const parent: { [key in number]: TreeNode } = {}
+
+  // find parent 
+  const dfs = (node, p) => {
+    if (!node) return;
+
+    parent[node.val] = p;
+
+    dfs(node.left, node);
+    dfs(node.right, node);
+  };
+  dfs(root, null);
+
+  const ans = [];
+  const vised = {}
+  const recDist = (node, lastK) => {
+    if (!node || vised[node.val]) return;
+    if (lastK === 0)
+      ans.push(node.val);
+
+    vised[node.val] = true;
+
+    recDist(node.left, lastK - 1);
+    recDist(node.right, lastK - 1);
+    recDist(parent[node.val], lastK - 1);
+  }
+  recDist(target, k)
+
+  return ans;
+};
