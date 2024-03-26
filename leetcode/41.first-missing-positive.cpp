ee23fce76b2f95a794406ae0a4a2c601f2fb8ae3
Phisten <ans10528@gmail.com>
2024-03-26 15:23:48 +0800

    OA lc cpp s41 O(n)/O(1)

--- leetcode/41.first-missing-positive.cpp
+++ leetcode/41.first-missing-positive.cpp
@@ -5,18 +5,62 @@
  */
 
 // @lc code=start
-class Solution {
+class Solution
+{
 public:
-    int firstMissingPositive(vector<int>& nums) {
+    int firstMissingPositive(vector<int> &nums)
+    {
+        int hasMissing = false;
+        int n = nums.size();
+
+        for (int i = 0; i < n; i++)
+        {
+            while (nums[i] != i + 1 && nums[i] != i + 1)
+            {
+                int v = nums[i];
+                if (v < 1 || v > n)
+                {
+                    break;
+                }
+                else
+                {
+                    if (v == nums[v - 1])
+                        break;
+                    nums[i] = nums[v - 1];
+                    nums[v - 1] = v;
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            int v = nums[i];
+            if (v != i + 1)
+                return i + 1;
+        }
+
+        return n + 1;
+    }
+};
+// @lc code=end
+
+class Solution_ht_nSpace
+{
+public:
+    int firstMissingPositive(vector<int> &nums)
+    {
         unordered_map<int, bool> ht;
 
-        for (int i = 0; i < nums.size(); i++) {
+        for (int i = 0; i < nums.size(); i++)
+        {
             int v = nums[i];
             ht[v] = true;
         }
 
-        for (int i = 1; i <= nums.size(); i++) {
-            if (!ht[i]) {
+        for (int i = 1; i <= nums.size(); i++)
+        {
+            if (!ht[i])
+            {
                 return i;
             }
         }
@@ -24,4 +68,3 @@ public:
         return nums.size() + 1;
     }
 };
-// @lc code=end
