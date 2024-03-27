3d0879adc17819494c91eef3565884a837d4ba80
Phisten <ans10528@gmail.com>
2024-03-28 05:17:52 +0800

    OA lc cpp s713 tle

---
+++ leetcode/713.subarray-product-less-than-k.cpp
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode id=713 lang=cpp
+ *
+ * [713] Subarray Product Less Than K
+ */
+
+// @lc code=start
+class Solution_TLE
+{
+public:
+    int numSubarrayProductLessThanK(vector<int> &nums, int k)
+    {
+        int n = nums.size();
+        int ans = 0;
+        vector<int> cache(nums);
+        cache.assign(n, 1);
+
+        for (int len = 1; len <= n; len++)
+        {
+            for (int i = 0; i < n - len + 1; i++)
+            {
+                if (cache[i] < k)
+                {
+                    cache[i] *= nums[i + len - 1];
+                    if (cache[i] < k)
+                    {
+                        ans++;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+};
+// @lc code=end
