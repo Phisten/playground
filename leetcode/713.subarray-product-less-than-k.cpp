d1bf701db2f88b4a734842aff797e431706e5f9c
Phisten <ans10528@gmail.com>
2024-03-28 05:37:41 +0800

    OA lc cpp s713 2 pointer

--- leetcode/713.subarray-product-less-than-k.cpp
+++ leetcode/713.subarray-product-less-than-k.cpp
@@ -5,6 +5,42 @@
  */
 
 // @lc code=start
+class Solution
+{
+public:
+    int numSubarrayProductLessThanK(vector<int> &nums, int k)
+    {
+        int n = nums.size();
+        int ans = 0;
+        vector<int> cache(nums);
+        cache.assign(n, 1);
+
+        int mpl = 1;
+        int rangeAns = 0;
+        for (int r = 0, l = 0; r < n; r++)
+        {
+            mpl *= nums[r];
+            if (mpl < k)
+            {
+                ans += r - l + 1;
+            }
+
+            while (l <= r && mpl >= k)
+            {
+                mpl /= nums[l];
+                l++;
+                if (mpl < k)
+                {
+                    ans += r - l + 1;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
+// @lc code=end
+
 class Solution_TLE
 {
 public:
@@ -32,4 +68,3 @@ public:
         return ans;
     }
 };
-// @lc code=end
