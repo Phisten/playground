607d7f73991b6b574d3e9fb94aed4fb9a7a2f02c
Phisten <ans10528@gmail.com>
2024-03-28 13:04:38 +0800

    OA lc cpp s2958 2 pointer

--- leetcode/2958.length-of-longest-subarray-with-at-most-k-frequency.cpp
+++ leetcode/2958.length-of-longest-subarray-with-at-most-k-frequency.cpp
@@ -8,8 +8,43 @@
 class Solution {
 public:
     int maxSubarrayLength(vector<int>& nums, int k) {
-        
+        int n = nums.size();
+        unordered_map<int, int> ht;
+        int valid = 0;
+        int subLen = 0;
+        int ans = 0;
+
+        for (int r = 0, l = 0; r < n; r++) {
+            int v = nums[r];
+            ht[v] += 1;
+
+            if (ht[v] == 1) {
+                subLen++;
+                valid++;
+            } else if (ht[v] == k+1) {
+                valid--;
+            }
+            if (subLen == valid) {
+                ans = max(ans, r - l + 1);
+            }
+
+            while (l <= r && subLen != valid) {
+                int vL = nums[l];
+                ht[vL] -= 1;
+                l++;
+                
+                if (ht[vL] == 0) {
+                    subLen--;
+                    valid--;
+                } else if (ht[vL] == k) {
+                    valid++;
+                }
+                if (subLen == valid) {
+                    ans = max(ans, r - l + 1);
+                }
+            }
+        }
+        return ans;
     }
 };
 // @lc code=end
-
