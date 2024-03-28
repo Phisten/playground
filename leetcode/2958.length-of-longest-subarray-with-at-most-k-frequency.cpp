39ca9bb8b818a8274654e6f81c88ccf51d1ad386
Phisten <ans10528@gmail.com>
2024-03-28 11:41:36 +0800

    OA lc cpp s2958 2 pointer

---
+++ leetcode/2958.length-of-longest-subarray-with-at-most-k-frequency.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=2958 lang=cpp
+ *
+ * [2958] Length of Longest Subarray With at Most K Frequency
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int maxSubarrayLength(vector<int>& nums, int k) {
+        
+    }
+};
+// @lc code=end
+
