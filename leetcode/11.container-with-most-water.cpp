8016f4485fd9d5b051d80d8cf1cdb821efa887f2
Phisten <ans10528@gmail.com>
2024-03-30 03:33:50 +0800

    OA lc cpp s11 2pointer

--- leetcode/11.container-with-most-water.cpp
+++ leetcode/11.container-with-most-water.cpp
@@ -10,6 +10,25 @@ class Solution
 public:
     int maxArea(vector<int> &height)
     {
+        int n = height.size();
+        int l = 0, r = n - 1;
+        int ans = 0;
+
+        while (l < r)
+        {
+            ans = max(ans, (r - l) * min(height[l], height[r]));
+
+            if (height[l] < height[r])
+            {
+                l++;
+            }
+            else
+            {
+                r--;
+            }
+        }
+
+        return ans;
     }
 };
 // @lc code=end
