f68911837fb99305cca6321f1f6c82cd5b1b8cfe
Phisten <ans10528@gmail.com>
2024-04-04 05:28:14 +0800

    OA lc cpp s79 daily

--- leetcode/79.word-search.cpp
+++ leetcode/79.word-search.cpp
@@ -5,40 +5,34 @@
  */
 
 // @lc code=startclass Solution {
-public:
-bool exist(vector<vector<char>> &board, string word)
+class Solution
 {
-    int m = board.size();
-    int n = board[0].size();
-    int len = word.length();
-
-    function<bool(int, int, int)> match = [&](int i, int j, int idx)
+public:
+    bool exist(vector<vector<char>> &board, string word)
     {
-        if (idx == len)
-            return true;
-        if (i < 0 || i >= m || j < 0 || j >= n)
-            return false;
-        bool res = false;
+        int m = board.size(), n = board[0].size(), len = word.length();
 
-        if (board[i][j] == word[idx])
+        function<bool(int, int, int)> match = [&](int i, int j, int idx)
         {
-            board[i][j] = 0;
-            res = match(i - 1, j, idx + 1) ||
-                  match(i, j - 1, idx + 1) ||
-                  match(i + 1, j, idx + 1) ||
-                  match(i, j + 1, idx + 1);
-            board[i][j] += word[idx];
-        }
-        return res;
-    };
-
-    for (int i = 0; i < m; i++)
-        for (int j = 0; j < n; j++)
-            if (match(i, j, 0))
+            if (idx == len)
                 return true;
+            if (i < 0 || i >= m || j < 0 || j >= n || board[i][j] != word[idx])
+                return false;
+            board[i][j] = 0;
+            bool res = match(i - 1, j, idx + 1) ||
+                       match(i, j - 1, idx + 1) ||
+                       match(i + 1, j, idx + 1) ||
+                       match(i, j + 1, idx + 1);
+            board[i][j] += word[idx];
+            return res;
+        };
 
-    return false;
-}
-}
-;
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++)
+                if (match(i, j, 0))
+                    return true;
+
+        return false;
+    }
+};
 // @lc code=end
