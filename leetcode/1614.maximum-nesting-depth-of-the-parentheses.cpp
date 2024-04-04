e3e1acbab8e85dd746cd1d331e075c4b2ecab7cf
Phisten <ans10528@gmail.com>
2024-04-04 23:40:04 +0800

    OA lc cpp s1614 daily

---
+++ leetcode/1614.maximum-nesting-depth-of-the-parentheses.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=1614 lang=cpp
+ *
+ * [1614] Maximum Nesting Depth of the Parentheses
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int maxDepth(string s) {
+        
+    }
+};
+// @lc code=end
+
