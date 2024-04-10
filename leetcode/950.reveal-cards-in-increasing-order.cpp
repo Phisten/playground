4c4621fa4a78637ed48c62efff8b11cec745d850
Phisten <ans10528@gmail.com>
2024-04-10 11:48:16 +0800

    OA lc cpp s950 daily 2

--- leetcode/950.reveal-cards-in-increasing-order.cpp
+++ leetcode/950.reveal-cards-in-increasing-order.cpp
@@ -5,32 +5,76 @@
  */
 
 // @lc code=start
+
+class Solution
+{
+public:
+    vector<int> deckRevealedIncreasing(vector<int> &deck)
+    {
+        int n = deck.size();
+        vector<int> ans(n, 0);
+        sort(deck.begin(), deck.end());
+
+        for (int i = 0, j = 0, skip = 0; i < n; i++)
+        {
+            while (true)
+            {
+                if (0 == ans[j])
+                {
+                    if (0 == skip)
+                    {
+                        ans[j] = deck[i];
+                        skip = 1;
+                        break;
+                    }
+                    else
+                    {
+                        skip = 0;
+                    }
+                }
+
+                j = (j + 1) % n;
+            }
+        }
+
+        return ans;
+    }
+};
+// @lc code=end
+
 /*
 2 3 5 7 B D G  increasing order
 2 D 3 B 5 G 7  ans
 
 1 2 3 4 5 6 7
-1 6 2 5 3 7 4 
+1 6 2 5 3 7 4
 
 1.照順序每填一個數字後就skip一個位置
 2.填完一輪再回到最前面空位重複1
 */
 
-class Solution {
+class Solution_2
+{
 public:
-    vector<int> deckRevealedIncreasing(vector<int>& deck) {
+    vector<int> deckRevealedIncreasing(vector<int> &deck)
+    {
         int n = deck.size();
         vector<int> ans(n, 0);
         sort(deck.begin(), deck.end());
 
-        for (int top = 0, skip = 0; top < n; ) {
-            for (int j = 0; j < n; j++) {
-                if (0 == ans[j]) {
-                    if (0 == skip) {
+        for (int top = 0, skip = 0; top < n;)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (0 == ans[j])
+                {
+                    if (0 == skip)
+                    {
                         ans[j] = deck[top];
                         top++;
                         skip = 1;
-                    } else
+                    }
+                    else
                         skip = 0;
                 }
             }
@@ -39,4 +83,3 @@ public:
         return ans;
     }
 };
-// @lc code=end
