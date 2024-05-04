7aac68e8189b62f186637f2b90e7368e54893cac
Phisten <ans10528@gmail.com>
2024-05-05 05:54:40 +0800

    nestjs add e2e script and setup script

---
+++ web/nestjs/car-value/test/auth.e2e-spec.ts
@@ -0,0 +1,34 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from './../src/app.module';
+
+describe('auth (e2e)', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it('handles a signup request', () => {
+    const email = '1222ss32123@gmail.com';
+
+    return request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({
+        email,
+        pwd: 'ada3sd22',
+      })
+      .expect(201)
+      .then((res) => {
+        const { id, email } = res.body;
+        expect(id).toBeDefined();
+        expect(email).toEqual(email);
+      });
+  });
+});
