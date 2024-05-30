47e5a69bea7167f941784d28e8b7cb5d971176a5
Phisten <ans10528@gmail.com>
2024-05-31 04:07:10 +0800

    OA lc cpp s1306 bfs

--- leetcode/1306.jump-game-iii.cpp
+++ leetcode/1306.jump-game-iii.cpp
@@ -11,6 +11,39 @@ public:
         int n = arr.size();
         if (n == 1) return true;
 
+        deque<int> dq;
+        dq.push_back(start);
+
+        while (dq.size() > 0) {
+            int cur = dq.front();
+            dq.pop_front();
+
+            if (cur >= n || cur < 0)
+                continue;
+            else if (arr[cur] == 0) {
+                return true;
+            } 
+
+            int curValue = arr[cur];
+            if (curValue != -1) {
+                arr[cur] = -1;
+                dq.push_back(cur + curValue);
+                dq.push_back(cur - curValue);
+            }
+        }
+
+        return false;
+    }
+};
+// @lc code=end
+
+
+class Solution_dfs {
+public:
+    bool canReach(vector<int>& arr, int start) {
+        int n = arr.size();
+        if (n == 1) return true;
+
         bool ans = false;
         function<void(int)> dfs = [&](int cur) {
             if (cur >= n || cur < 0)
@@ -32,5 +65,3 @@ public:
         return ans;
     }
 };
-// @lc code=end
-
