b0b6bf97201ea579a6c5e57658c3ff4279e2e946
Phisten <ans10528@gmail.com>
2024-06-27 12:45:59 +0800

    fix: ts utils excludeNullItems

--- web/libs/utils/excludeNullItems.ts
+++ web/libs/utils/excludeNullItems.ts
@@ -1,3 +1,3 @@
-// fix for "typescript": "~5.3.3"
+// fix for typescript <5.5
 export const excludeNullItems = <T>(arr: (T | null)[]) =>
   arr.filter((v: T | null) => v !== null) as Exclude<T, null>[];
