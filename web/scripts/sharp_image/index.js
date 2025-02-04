d86b39c34882015ad9f204db205f0f8c696268c9
Phisten <ans10528@gmail.com>
2025-02-04 16:46:14 +0800

    sharp update

--- web/scripts/sharp_image/index.js
+++ web/scripts/sharp_image/index.js
@@ -3,7 +3,8 @@ const path = require("path");
 const sharp = require("sharp");
 
 // 压缩图片的目标文件夹
-const outputFolder = "./compressed_images";
+const outputFolder = path.join(process.env.HOME, "downloads/sharp");
+// const outputFolder = "./compressed_images";
 
 // 确保输出文件夹存在
 if (!fs.existsSync(outputFolder)) {
@@ -28,15 +29,25 @@ const imageFiles = files.filter((file) => {
 imageFiles.forEach((file) => {
   const inputPath = path.join(__dirname, inputFolder, file);
   const outputPath = path.join(
-    __dirname,
     outputFolder,
-    file
+
+    // file
+    path.parse(file).name + "." + "png"
     // path.parse(file).name + "." + "jpg"
+    // path.parse(file).name + "." + "webp"
   );
+
+  const outputType = outputPath.split(".").at(-1);
+  const sharpTypeName = {
+    jpg: "jpeg",
+    png: "png",
+    webp: "webp",
+  }[outputType];
+
   sharp(inputPath)
-    .resize({ width: 320 }) // 设置压缩后的宽度，可根据需要调整
+    ?.[sharpTypeName]()
+    // .resize({ width: 1280 }) // 设置压缩后的宽度，可根据需要调整
     // .resize({ width: 400, height: 400 })
-    // .jpeg()
     .toFile(outputPath, (err, info) => {
       if (err) {
         console.error(`Failed to compress ${file}: ${err}`);
